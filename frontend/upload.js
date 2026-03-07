392ef168eba9bf7ef9a09bdbe3ceebfb23adbf0f
Uzair <uzairhussain98@hotmail.com>
2026-03-07 07:32:09 -0600

    added dashboard styles

---
+++ frontend/upload.js
@@ -0,0 +1,21 @@
+async function uploadFile() {
+
+    const fileInput = document.getElementById("fileInput");
+    const file = fileInput.files[0];
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const response = await fetch("http://127.0.0.1:8000/upload/", {
+        method: "POST",
+        body: formData
+    });
+
+    const data = await response.json();
+
+    // Save parsed data
+    localStorage.setItem("metsData", JSON.stringify(data));
+
+    // Redirect to summary page
+    // window.location.href = "summary.html";
+}
